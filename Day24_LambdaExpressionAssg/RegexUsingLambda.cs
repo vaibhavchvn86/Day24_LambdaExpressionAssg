230f80bbb7ee82db5e3fea86f69cc56d3ebd5ce2
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-11-08 13:11:49 +0530

    [VaibhavChavan]Added:Validating FirstName Using Lambda Function

---
+++ Day24_LambdaExpressionAssg/RegexUsingLambda.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day24_LambdaExpressionAssg
+{
+    public class RegexusingLambda
+    {
+        /// <summary>
+        ///UC13 Refactor the Code to use Lambda Function
+        /// </summary>
+        public static string pattern = "^[A-Z][a-z]{2,}$";
+        public bool validateFirstName(string Name)
+        {
+            Regex name = new Regex(pattern);
+            bool Fn = name.IsMatch(Name);
+            return Fn;
+        }
+    }
+}
