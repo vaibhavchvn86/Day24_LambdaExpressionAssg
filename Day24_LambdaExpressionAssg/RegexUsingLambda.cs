3e2d77ab170ad556d19eb4fbcac2a7cd583aac1f
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-11-08 13:33:42 +0530

    [VaibhavChavan]Added: Validating Email Using Lambda Function

---
+++ Day24_LambdaExpressionAssg/RegexUsingLambda.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day24_LambdaExpressionAssg
+{
+    class RegexUsingLambda
+    {
+        public static string Regex_Email = "^[a-zA-Z0-9]+([.#_$+-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2})?$";
+
+        /// <summary>
+        /// Validates the email.
+        /// </summary>
+        /// <param name="Email">The email.</param>
+        /// <returns></returns>
+        public bool validateEmail(string Email)
+        {
+            Regex mail = new Regex(Regex_Email);
+            bool email = mail.IsMatch(Email);
+            return email;
+        }
+        public static Func<string, bool> validateUserEmail = Email => Regex.IsMatch(Email, Regex_Email);
+    }
+}
