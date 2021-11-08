7eda0580224cfec647018cbdeda8b46041afa55a
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-11-08 13:17:04 +0530

    [VaibhavChavan]Added:Validating LastName Using Lambda Function

---
+++ Day24_LambdaExpressionAssg/RegexUsingLambda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day24_LambdaExpressionAssg
+{
+    class RegexUsingLambda
+    {
+        public static string pattern = "^[A-Z][a-z]{2,}$";
+        public bool validateLastName(string Name)
+        {
+            Regex name = new Regex(pattern);
+            bool Fn = name.IsMatch(Name);
+            return Fn;
+        }
+    }
+}
