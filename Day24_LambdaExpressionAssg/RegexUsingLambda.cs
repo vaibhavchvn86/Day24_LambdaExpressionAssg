741046b50f66d164f0f8be627d47fecf519cd0fe
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-11-08 13:40:32 +0530

    [VaibhavChavan]Added: Validating Password Using Lambda Function

---
+++ Day24_LambdaExpressionAssg/RegexUsingLambda.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Day24_LambdaExpressionAssg
+{
+    class RegexUsingLambda
+    {
+        public static string Regex_password = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$";
+
+        /// <summary>
+        /// Validates the password.
+        /// </summary>
+        /// <param name="Pass">The pass.</param>
+        /// <returns></returns>
+        public bool validatePassword(string Pass)
+        {
+            Regex pass = new Regex(Regex_password);
+            bool password = pass.IsMatch(Pass);
+            return password;
+        }
+        public static Func<string, bool> validateuserpassword = Pass => Regex.IsMatch(Pass, Regex_password);
+    }
+}
